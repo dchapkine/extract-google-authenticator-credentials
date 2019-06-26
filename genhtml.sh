63a2e6370c3ed9ab8af155c3f3598b4d1ff75e2d
Pierre Schefler <p.schefler@lancey.fr>
2019-06-27 00:58:18 +0200

    Added support for HOTP URLs

--- genhtml.sh
+++ genhtml.sh
@@ -13,8 +13,11 @@ IFS=";"
 while read _id email secret counter type provider issuer original_name
  do
 
-
-	url="otpauth://totp/"$email"?secret="$secret"&issuer="$issuer;
+	if [ "$type" == "1" ]; then
+		url="otpauth://hotp/"$email"?secret="$secret"&issuer="$issuer"&counter="$counter;
+	else
+		url="otpauth://totp/"$email"?secret="$secret"&issuer="$issuer;
+	fi
 	echo '<div class="qrcode">'
 	echo '<div class="title">'$email'</div>'
 	echo '<div class="code" id="__qrcode__'$_id'"></div>'
@@ -25,9 +28,12 @@ while read _id email secret counter type provider issuer original_name
 
     #echo -e "$_id \
     #======================\n\
+    #Type: \t $type\n\
     #Email :\t $email\n\
     #Secret :\t $secret\n\
     #Issuer :\t $issuer\n\
+    #Counter:\t $counter\n\
+    #Url:\t $url\n\
     #\n"
 
  done < $1
